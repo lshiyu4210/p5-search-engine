efc01ce7444ffa5aa128de90f3b0f73cc31f349d
lshiyu <lshiyu@umich.edu>
2024-04-15 23:51:22 -0400

    23.51

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 """Map 0."""
-print('hello world')
+import sys
+import re
+
+def main():
+    pattern = re.compile(r'<!DOCTYPE html>', re.IGNORECASE)
+    for line in sys.stdin:
+        if pattern.search(line):
+            print('DocCount\t1')
+
+if __name__ == "__main__":
+    main()
--- inverted_index/reduce0.py
+++ inverted_index/reduce0.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 """Reduce 0."""
+import sys
+
+def main():
+    total_count = 0
+    for line in sys.stdin:
+        key, value = line.strip().split('\t')
+        if key == "DocCount":
+            total_count += int(value)
+    print(total_count)
+
+if __name__ == "__main__":
+    main()
