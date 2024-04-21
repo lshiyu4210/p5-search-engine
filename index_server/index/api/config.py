d0eebe6a632d35544ab170172ca44a8c714973eb
Liam Gai <zgai@umich.edu>
2024-04-21 19:32:41 -0400

    slight modification

--- index_server/index/api/config.py
+++
@@ -1,8 +0,0 @@
-"""Global variables."""
-from collections import defaultdict
-# Define global variables here
-INDICES = defaultdict(dict)
-PAGERANK = {}
-STOPWORDS = []
-
-print("Inverted Indexes, PageRank, and Stopwords created.")
--- index_server/index/api/load_index.py
+++ index_server/index/api/load_index.py
@@ -2,18 +2,26 @@
 import index
 
 stopwords_path = index.app.config["FILE_PATH"] / 'stopwords.txt'
-PAGERANK_PATH = index.app.config["FILE_PATH"] / 'pagerank.out'
+pagerank_path = index.app.config["FILE_PATH"] / 'pagerank.out'
 index_path = index.app.config["INDEX_DICT_PATH"]
 
 
 def load_index():
     """Load the index file into memory."""
+    
+    #  Load the stopword files into memory 
     with open(stopwords_path, 'r', encoding='utf-8') as file:
         index.stopwords_set = set(file.read().split())
 
     temp_dict = {}
-    with open(PAGERANK_PATH, 'r', encoding='utf-8') as file:
+    #  load the pagerank file into memory
+    with open(pagerank_path, 'r', encoding='utf-8') as file:
         index.pagerank_list = [line.strip() for line in file.readlines()]
+
+    #  load the inverted index
+    with open(index_path, 'r', encoding='utf-8') as file:
+        index.index_list = [line.strip() for line in file.readlines()]
+        
     for line in index.pagerank_list:
         doc_id, pagerank = line.split(',')[0], line.split(',')[1]
         temp_dict[doc_id] = pagerank
@@ -21,19 +29,17 @@ def load_index():
 
     temp_dict = {}
     doc_n_factor = {}
-    with open(index_path, 'r', encoding='utf-8') as file:
-        index.index_list = [line.strip() for line in file.readlines()]
 
     for line in index.index_list:
-        line_list = line.split()
-        term = line_list[0]
-        term_idf = line_list[1]
-        temp_dict[term] = {'idf': term_idf}
+        entry = line.split()
+        term = entry[0]
+        idf = entry[1]
+        temp_dict[term] = {'idf': idf}
 
-        for i in range(2, len(line_list), 3):
-            doc_id = line_list[i]
-            term_freq = line_list[i + 1]
-            n_factor = line_list[i + 2]
+        for i in range(2, len(entry), 3):
+            doc_id = entry[i]
+            term_freq = entry[i + 1]
+            n_factor = entry[i + 2]
             doc_n_factor[doc_id] = n_factor
             temp_dict[term][doc_id] = term_freq
 
