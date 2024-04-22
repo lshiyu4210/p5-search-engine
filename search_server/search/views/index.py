3d8fe008c9e94e5c8656265939710b95a60c4cc2
lshiyu <lshiyu@umich.edu>
2024-04-21 21:06:16 -0400

    pylint search

--- search_server/search/views/index.py
+++ search_server/search/views/index.py
@@ -4,24 +4,26 @@ Search Server index (main) view.
 URLs include:
 /
 """
-from flask import render_template, request
 import threading
-import requests
 from heapq import merge
+from flask import render_template, request
+import requests
 import search
 
 def fetch_hits(url, query_params, result_list):
+    """Fetch hits."""
     try:
-        response = requests.get(url, params=query_params)
+        response = requests.get(url, params=query_params, timeout=10)
         response.raise_for_status()
-        result_list.extend(response.json()['hits'])  # Use extend instead of append
+        result_list.extend(response.json()['hits'])
     except requests.RequestException as e:
         print(f"Error fetching results from {url}: {e}")
 
 def get_top_10_results(query, weight):
+    """Get top 10 results from index server."""
     query_params = {'q': query, 'w': weight}
     threads = []
-    results = [[] for _ in range(len(search.app.config["SEARCH_INDEX_SEGMENT_API_URLS"]))]  # A list of lists to store results for each thread
+    results = [[] for _ in range(len(search.app.config["SEARCH_INDEX_SEGMENT_API_URLS"]))]
 
     # Start a new thread for each index server URL
     for i, url in enumerate(search.app.config["SEARCH_INDEX_SEGMENT_API_URLS"]):
@@ -53,7 +55,11 @@ def show_index():
         # Retrieve website details from the database
         connection = search.model.get_db()
         placeholders = ', '.join('?' for unused in top_hits)
-        query_string = f'SELECT docid, title, summary, url FROM documents WHERE docid IN ({placeholders})'
+        query_string = (
+            f'SELECT docid, title, summary, url '
+            f'FROM documents '
+            f'WHERE docid IN ({placeholders})'
+        )
         cur = connection.execute(query_string, [hit['docid'] for hit in top_hits])
         websites_dict = {row['docid']: row for row in cur.fetchall()}
 
@@ -65,5 +71,5 @@ def show_index():
 
         # Use the results to render the template
         return render_template('index.html', query=query, weight=weight, websites=websites)
-    else:
-        return render_template('index.html', query='', weight=weight, no_results=False, websites=[])
+
+    return render_template('index.html', query='', weight=weight, no_results=False, websites=[])
